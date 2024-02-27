b6657c9fb0d88d31088fc778ddaec7a691c9c59d
IsaiahSama <zeldaplayergl@gmail.com>
2024-02-27 14:05:29 -0400

    Added page where main game will be

--- Frontend/scripts/script.js
+++ Frontend/scripts/script.js
@@ -1,3 +1,7 @@
 const promptForRoomCode = () => {
     document.getElementById("helpText").innerHTML = "If you have a room code, enter it below, otherwise create your own room!"
 }
+
+const joinSomeRoom = (e) => {
+    window.location.href = "/game.html";
+}
