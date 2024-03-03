e1eb28077acf412a8ace4cb1892749757a2d7d3c
IsaiahSama <zeldaplayergl@gmail.com>
2024-03-03 09:34:54 -0400

    will now store room code

--- Frontend/scripts/sockets.js
+++ Frontend/scripts/sockets.js
@@ -2,6 +2,22 @@ const notif = document.getElementById("notification")
 
 const socket = io("http://127.0.0.1:8000/")
 
+const setupRoom = () => {
+    let url = window.location.search;
+    let getQuery = url.split("?")[1]
+    let params = getQuery.split("&")
+    
+    for (let param of params){
+        const [key, value] = param.split('=')
+        if (key == "roomCode"){
+            addLocalEntry(key, value);
+            break
+        }
+    }
+}
+
+setupRoom()
+
 socket.on("room", (data) => {
     notif.innerHTML = data['text'];
 })
